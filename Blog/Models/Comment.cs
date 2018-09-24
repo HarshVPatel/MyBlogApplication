36a5254e5e701704343f3ee2eed7aed6fe00ba51
HarshVPatel <37636885+HarshVPatel@users.noreply.github.com>
2018-09-24 02:08:44 -0500

    Add project files.

---
+++ Blog/Models/Comment.cs
@@ -0,0 +1,28 @@
+﻿using Blog.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Blog.Controllers
+{
+    public class Comment
+    {
+        public int Id { get; set; }
+
+        public int PostId { get; set; }
+
+        public string AuthorId { get; set; }
+
+        public string Body { get; set; }
+
+        public DateTimeOffset Created { get; set; }
+
+        public DateTimeOffset? Updated { get; set; }
+
+        public string UpdateReason { get; set; }
+
+        public virtual ApplicationUser Author { get; set; }
+
+    }
+}
